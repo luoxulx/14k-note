8c26e102ce4acc9b202cdf40bc5798a145f14294
Jason.Roy <35016570+luoxulx@users.noreply.github.com>
2018-07-23 12:02:43 +0800

    Update install-note.sh

--- install-note.sh
+++ install-note.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+#add DATA disk
+fdisk -l
+fdisk /dev/vdb1
+n，p，1，'enter'，'enter'，w
+fdisk -l
+makdir /xxx
+mkfs -t ext3 /dev/vdb1 
+mount /dev/vdb1 /xxx/
+df -h
+blkid
+vi /etc/fstab
+'/dev/vdbl /aiWn eac4 defaulcg 0 0'
+
 #google bbr and update to atleast 4.9
 wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
 chmod +x bbr.sh
