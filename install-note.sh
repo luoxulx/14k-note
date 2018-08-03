d859989e1398d00abdd3779275044e0050f18c41
Jason.Roy <35016570+luoxulx@users.noreply.github.com>
2018-08-03 14:39:52 +0800

    Update install-note.sh

--- install-note.sh
+++ install-note.sh
@@ -1,5 +1,12 @@
-'''
 #swap
 free -m
 swapon -s
-'''
+df -hal
+dd if=/dev/zero of=/swapfile bs=1024 count=512k
+mkswap /swapfile
+swapon /swapfile
+swapon -s
+vim /etc/fstab
+add '/swapfile          swap            swap    defaults        0 0'
+chown root:root /swapfile
+chmod 0600 /swapfile
