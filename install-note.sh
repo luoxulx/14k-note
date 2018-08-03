99ef77e72c9c0b6f5391a2f009c9c3344d2c8ee9
Jason.Roy <35016570+luoxulx@users.noreply.github.com>
2018-08-03 14:47:03 +0800

    Update install-note.sh

--- install-note.sh
+++ install-note.sh
@@ -10,3 +10,18 @@ vim /etc/fstab
 add '/swapfile          swap            swap    defaults        0 0'
 chown root:root /swapfile
 chmod 0600 /swapfile
+#.vimrc --config
+set nu
+set shortmess=atI
+syntax on
+set nobackup
+set confirm
+set mouse=a
+set tabstop=4
+set shiftwidth=4
+set autoread
+set hlsearch
+"set background=dark
+set showmatch
+set ruler
+autocmd InsertEnter * se cul
